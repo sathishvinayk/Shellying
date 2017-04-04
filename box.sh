702d044eea07398aa6e3a0f491b2344ca5467f39
sathishvinayk <sathishvinayk@gmail.com>
2017-04-04 22:06:54 +0530

    Added indic kb static page

--- box.sh
+++ box.sh
@@ -1,18 +1,22 @@
 function box_out()
 {
-  prd="*"
+  prd="**"
   local s=("$@") b w
   for l in "${s[@]}"; do
     ((w<${#l})) && { b="$l"; w="${#l}"; }
   done
   tput setaf 3
-  echo " -${b//?/-}Values and their properties-----------
-| ${b//?/ }ja: Japanese $prd ko: Korean            | "
+  echo " -${b//?/-}Values and their properties-----------------------------------------------
+| ${b//?/ }   *********  FONTS  ************    |  ********  INDIC KB  **********   |
+| ${b//?/ }ja => Japanese                       |   hi  => Hindi KB                 | "
   for l in "${s[@]}"; do
     printf '| %s%*s%s |\n' "$(tput setaf 4)" "-$w" "$l" "$(tput setaf 3)"
   done
-  echo "| ${b//?/ }cn: Chinese  $prd hi: Hindi Keyboard    |
- -${b//?/-}-------------------------------------- "
+  echo "| ${b//?/ }cn => Chinese                        |   mal => Malayalam KB             |
+| ${b//?/ }ko => Korean                         |   ta  => Tamil KB                 |
+| ${b//?/ }                                     |   gu  => Gujarati KB              |
+| ${b//?/ }                                     |   mar => Marathi KB               |
+ -${b//?/-}-------------------------------------------------------------------------- "
   tput sgr 0
 }
 box_out
